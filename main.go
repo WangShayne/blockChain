014c70523078442064087b5cf5726596aa2bfc53
shayne <1614565666@qq.com>
2019-07-08 14:24:48 +0800

    * 定义区块结构     * 前区块哈希     * 当前区块哈希     * 数据   * 创建区块   * 生成哈希   * 引入区块链

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+//	* 定义区块结构
+//	* 前区块哈希
+//	* 当前区块哈希
+//	* 数据
+type Block struct {
+	PrevHash []byte //前区块hash
+	Hash     []byte //当前区块hash
+	Data     []byte //数据
+}
+
+//    * 生成哈希
+func (block *Block) SetHash() {
+	// 获取区块信息
+	blockInfo := append(block.PrevHash, block.Data...)
+	// 生成hash
+	hash := sha256.Sum256(blockInfo)
+
+	block.Hash = hash[:]
+}
+
+//   * 创建区块
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := Block{
+		PrevHash: prevBlockHash,
+		Hash:     []byte{},
+		Data:     []byte(data),
+	}
+
+	block.SetHash()
+
+	return &block
+}
+
+//  创世区块
+func GenesisBlock() *Block {
+	return NewBlock("这是一个创世区块", []byte{})
+}
+
+//   * 引入区块链
+type BlockChain struct {
+	blocks []*Block
+}
+
+func NewBlockChain() *BlockChain {
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+
+func main() {
+	bl := NewBlockChain()
+	for i, block := range bl.blocks {
+		fmt.Printf("当前区块高度: %d\n", i)
+		fmt.Printf("前区块hash值: %x\n", block.PrevHash)
+		fmt.Printf("当前区块hash值: %x\n", block.Hash)
+		fmt.Printf("区块数据: %s\n", block.Data)
+	}
+
+}
