8d3e412b2f41dd903f893fca398c07faa7297dba
shayne <1614565666@qq.com>
2019-07-08 15:44:54 +0800

    7.uint转byte

--- block.go
+++ block.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
+	"log"
 	"time"
 )
 
@@ -49,7 +52,12 @@ func (block *Block) SetHash() {
 
 // uint64转byte
 func Uint64ToByte(num uint64) []byte {
-	return []byte{}
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buffer.Bytes()
 }
 
 //   * 创建区块
--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 )
 
-
 func main() {
 	bc := NewBlockChain()
 	bc.AddBlock("添加一个新区块")
 	bc.AddBlock("添加第二个新区块")
 	for i, block := range bc.blocks {
 		fmt.Printf("当前区块高度: %d\n", i)
+		fmt.Printf("时间 %d\n", block.Timestamp)
 		fmt.Printf("前区块hash值: %x\n", block.PrevHash)
 		fmt.Printf("当前区块hash值: %x\n", block.Hash)
 		fmt.Printf("区块数据: %s\n", block.Data)
